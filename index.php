2a47aea34c1caef7f17ab45a4afb87db5f1097a0
Guigu <Guigu29@gmail.com>
2019-05-20 13:54:39 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -2,19 +2,39 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-//	echo "OK";
-	$sql =new Hcode\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//	echo "OK";
+	//	$sql =new Hcode\DB\sql();
+	//	$results = $sql->select("SELECT * FROM tb_users");
+	//	echo json_encode($results);
+
+	$page = new Page();
+	$page->setTpl("index");
 
 
 });
+
+
+$app->get('/admin', function() {
+ 
+	$page = new PageAdmin();
+	$page->setTpl("index");
+
+
+});
+
+
+
 
 $app->run();
 
